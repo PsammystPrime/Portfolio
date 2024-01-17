9a158609f2da03aeefb9253cdad152392eeead04
PsammystPrime <samuelprime384@gmail.com>
2024-01-17 19:21:16 +0300

    Update blogs.js

--- blogs.js
+++ blogs.js
@@ -1,8 +1,23 @@
 const display = document.querySelector('.display')
 
+const blog = document.createElement('div')
+const blogPic = document.createElement('img')
+const blogText = document.createElement('p')
+
+blog.className = 'blog'
+blogPic.className = 'blogPic'
+blogPic.src = './pic.png'
+blogText.className = 'blogText'
+blogText.textContent = 'Javascript Arrays...'
+
+blog.appendChild(blogPic)
+blog.appendChild(blogText)
+
 function updateBlogs (){
     //return console.log('alive too')
     display.textContent = ''
+
+    display.appendChild(blog)
 }
 
 export{updateBlogs}
