bd678a9862004c76f0a752905f1918185339f4c2
PsammystPrime <samuelprime384@gmail.com>
2024-01-21 22:13:26 +0300

    Add info text on hover

--- projects.js
+++ projects.js
@@ -30,6 +30,41 @@ blogCard.appendChild(blogText)
 blog.appendChild(blogPic)
 blog.appendChild(blogCard)
 
+const blogObjective = document.createElement('div')
+const objectiveHeader = document.createElement('h1')
+const objectiveText = document.createElement('p')
+const blogStack = document.createElement('div')
+const stackHeader = document.createElement('h1')
+const stack = document.createElement('ul')
+const stackList1 = document.createElement('li')
+const stackList2 = document.createElement('li')
+
+blogObjective.className = 'blogObjective'
+objectiveHeader.className = 'objectiveHeader'
+objectiveText.className = 'objectiveText'
+blogStack.className = 'blogStack'
+stackHeader.className = 'stackHeader'
+stack.className = 'stack'
+
+objectiveHeader.textContent = 'Project Objective'
+objectiveText.textContent = 'This is a Project solely devoted to use of grids in css as opposed to use of flexbox'
+stackHeader.textContent = 'Tech-Stack'
+stackList1.textContent = 'HTML5'
+stackList2.textContent = 'CSS'
+
+blogObjective.appendChild(objectiveHeader)
+blogObjective.appendChild(objectiveText)
+stack.appendChild(stackList1)
+stack.appendChild(stackList2)
+blogStack.appendChild(stackHeader)
+blogStack.appendChild(stack)
+
+blog.addEventListener('mouseover',()=> {
+    info.textContent = ''
+    info.appendChild(blogObjective)
+    info.appendChild(blogStack)
+})
+
 const blog2 = document.createElement('div')
 const blog2Pic = document.createElement('img')
 const blog2Card = document.createElement('div')
@@ -50,6 +85,41 @@ blog2.appendChild(blog2Card)
 
 projects.appendChild(blog)
 projects.appendChild(blog2)
+
+const blog2Objective = document.createElement('div')
+const objective2Header = document.createElement('h1')
+const objective2Text = document.createElement('p')
+const blog2Stack = document.createElement('div')
+const stack2Header = document.createElement('h1')
+const stack2 = document.createElement('ul')
+const stack2List1 = document.createElement('li')
+const stack2List2 = document.createElement('li')
+
+blog2Objective.className = 'blogObjective'
+objective2Header.className = 'objectiveHeader'
+objective2Text.className = 'objectiveText'
+blog2Stack.className = 'blogStack'
+stack2Header.className = 'stackHeader'
+stack2.className = 'stack'
+
+objective2Header.textContent = 'Project Objective'
+objective2Text.textContent = 'This is a Project solely devoted to use of grids in css as opposed to use of flexbox'
+stack2Header.textContent = 'Tech-Stack'
+stack2List1.textContent = 'HTML'
+stack2List2.textContent = 'CSS'
+
+blog2Objective.appendChild(objective2Header)
+blog2Objective.appendChild(objective2Text)
+stack2.appendChild(stack2List1)
+stack2.appendChild(stack2List2)
+blog2Stack.appendChild(stack2Header)
+blog2Stack.appendChild(stack2)
+
+blog2.addEventListener('mouseover',()=> {
+    info.textContent = ''
+    info.appendChild(blog2Objective)
+    info.appendChild(blog2Stack)
+})
 
 function UpdateProjects (){
     display.textContent = '';
