6d230298ec99166d20b8c429004d3255c51b0742
PsammystPrime <samuelprime384@gmail.com>
2024-01-24 19:57:32 +0300

    Add links to blogs

--- blogs.js
+++ blogs.js
@@ -4,11 +4,12 @@ blogContainer.className = 'blogContainer'
 
 const blog = document.createElement('div')
 const blogPic = document.createElement('img')
-// const blogCard = document.createElement('div')
+const blogLink = document.createElement('a')
 const blogHeader = document.createElement('h1')
 const blogText = document.createElement('p')
 
 blog.className = 'blog'
+blogLink.href = 'www.medium.com/aaa'
 blogPic.className = 'blogPic'
 blogPic.src = './freelance.png'
 blogText.className = 'blogText'
@@ -17,18 +18,19 @@ blogHeader.textContent = `Beginner's Quick Guide to gaining freelance skills usi
 blogText.textContent = `The world of freelance work and remote jobs has seen the emergence of a rapid 
 desire for technical sklls globally. Have you ever been asked for what type of job you'd like and in 
 response found yourself saying "...any job that is available"? Well, this article is for you...`
-
-blog.appendChild(blogHeader)
+blogLink.appendChild(blogHeader)
+blog.appendChild(blogLink)
 blog.appendChild(blogPic)
 blog.appendChild(blogText)
 
 
 const blog2 = document.createElement('div')
 const blog2Pic = document.createElement('img')
-// const blog2Card = document.createElement('div')
+const blog2Link = document.createElement('a')
 const blog2Header = document.createElement('h1')
 const blog2Text = document.createElement('p')
 blog2.className = 'blog'
+blog2Link.href = 'www.medium.com/dddd'
 blog2Pic.className = 'blogPic'
 blog2Pic.src = './arrays.jpg'
 blog2Text.className = 'blogText'
@@ -38,18 +40,19 @@ blog2Text.textContent = `Arrays are one of the fundamental concepts to learn whe
 Is getting started with programming. Understanding them and how they work is key to getting your feet off 
 the ground. This article has been drafted in the simplest way possible for effective understanding 
 regardless of intellectual background. ` 
-// blog2.appendChild(blog2Card)
-blog2.appendChild(blog2Header)
+blog2Link.appendChild(blog2Header)
+blog2.appendChild(blog2Link)
 blog2.appendChild(blog2Pic)
 blog2.appendChild(blog2Text)
 
 
 const blog3 = document.createElement('div')
 const blog3Pic = document.createElement('img')
-// const blog3Card = document.createElement('div')
+const blog3Link = document.createElement('a')
 const blog3Header = document.createElement('h1')
 const blog3Text = document.createElement('p')
 blog3.className = 'blog'
+blog3Link.href = 'www.medium.com/dddd'
 blog3Pic.className = 'blogPic'
 blog3Pic.src = './2024.jpg'
 blog3Text.className = 'blogText'
@@ -58,18 +61,20 @@ blog3Header.textContent = 'Writing Great Content in 2024'
 blog3Text.textContent = `Proven Ideas and methods for writers in enhancing writing skills. It will guide
 you through challenges a writer encounters and ways of overcomimg them ensuring you publish great content
 that stands out to readers.`
-blog3.appendChild(blog3Header)
+blog3Link.appendChild(blog3Header)
+blog3.appendChild(blog3Link)
 blog3.appendChild(blog3Pic)
 blog3.appendChild(blog3Text)
-// blog3.appendChild(blog3Card)
+
 
 
 const blog4 = document.createElement('div')
 const blog4Pic = document.createElement('img')
-// const blog4Card = document.createElement('div')
+const blog4Link = document.createElement('a')
 const blog4Header = document.createElement('h1')
 const blog4Text = document.createElement('p')
 blog4.className = 'blog'
+blog4Link.href = 'www.medium.com/dddd'
 blog4Pic.className = 'blogPic'
 blog4Pic.src = './comps.jpg'
 blog4Text.className = 'blogText'
@@ -77,18 +82,20 @@ blog4Header.className = 'blogHeader'
 blog4Header.textContent = `Web Apps vs Mobile Apps`
 blog4Text.textContent = `We explore the world of web Development and Mobile Development both for users and
  developers. Do's and Don'ts, expectations, and the factors that may necessitate one choice over another.`
-blog4.appendChild(blog4Header)
+blog4Link.appendChild(blog4Header)
+blog4.appendChild(blog4Link)
 blog4.appendChild(blog4Pic)
 blog4.appendChild(blog4Text)
-// blog4.appendChild(blog4Card)
+
 
 
 const blog5 = document.createElement('div')
 const blog5Pic = document.createElement('img')
-// const blog5Card = document.createElement('div')
+const blog5Link = document.createElement('a')
 const blog5Header = document.createElement('h1')
 const blog5Text = document.createElement('p')
 blog5.className = 'blog'
+blog5Link.href = 'www.medium.com/dddd'
 blog5Pic.className = 'blogPic'
 blog5Pic.src = './bundling.jpg'
 blog5Text.className = 'blogText'
@@ -97,17 +104,13 @@ blog5Header.textContent = `Bundling Modules with Webpack`
 blog5Text.textContent = `ES6 Modules are among the latest features in Javascript. This article will take you
 through benefits of bundling up project modules and resources using webpack and why you should incorporate
 such a tool for successful deployment of your project to production.`
-blog5.appendChild(blog5Header)
+blog5Link.appendChild(blog5Header)
+blog5.appendChild(blog5Link)
 blog5.appendChild(blog5Pic)
 blog5.appendChild(blog5Text)
-// blog5.appendChild(blog5Card)
 
-
-
-// blog.appendChild(blogCard)
 
 function updateBlogs (){
-    //return console.log('alive too')
     display.textContent = ''
 
     blogContainer.appendChild(blog)
