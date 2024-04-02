43f3435c2465895d44f73f91582b0fba9854ff5e
PsammystPrime <samuelprime384@gmail.com>
2024-04-02 09:30:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
-const home = document.querySelector('#home');
-const projects = document.querySelector('#projects');
-const blogs = document.querySelector('#blogs');
+const home = document.querySelector("#home");
+const projects = document.querySelector("#projects");
+const blogs = document.querySelector("#blogs");
 
-
-import { updateBlogs } from "./blogs.js";
-// import { UpdateProjects } from "./projects.js";
 import { updateHome } from "./home.js";
 
-// blogs.addEventListener('click', updateBlogs)
-// projects.addEventListener('click', )
-home.addEventListener('click', updateHome)
-
-updateHome()
-
-// function updateHome(){s
-    
-//     display.textContent=''
-
-//     function Blogs(image,title,preview){
-//         this.image=image
-//         this.title=title
-//         this.preview=preview
-//     }
-    
-
-
-
-
+home.addEventListener("click", updateHome());
